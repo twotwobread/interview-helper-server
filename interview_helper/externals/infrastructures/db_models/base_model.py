d5eb7ce583317e0ecad0118e785a40159a60cf9d
nika.lee <dltndud9137@naver.com>
2024-03-05 22:00:58 +0900

    feat: add db url

--- interview_helper/externals/infrastructures/db_models/base_model.py
+++ interview_helper/externals/infrastructures/db_models/base_model.py
@@ -1,7 +1,17 @@
 from datetime import datetime
 from typing import Optional
 
-from sqlmodel import Column, Field, Session, SQLModel, create_engine
+from sqlmodel import Column, Field, SQLModel, create_engine
+
+from configs.config import CONFIG
+
+POSTGRE_CONFIG = CONFIG["infra"]["postgre"]
+HOST = POSTGRE_CONFIG["host"]
+PORT = POSTGRE_CONFIG["port"]
+USER = POSTGRE_CONFIG["user"]
+PASSWORD = POSTGRE_CONFIG["pass"]
+DATABASE = POSTGRE_CONFIG["database"]
+DB_URL = f"postgresql+asyncpg://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}"
 
 
 class BaseModel(SQLModel):
@@ -10,5 +20,4 @@ class BaseModel(SQLModel):
     created_at: datetime = Field(default_factory=datetime.utcnow)
 
 
-engine = create_engine("sqlite:///db.sqlite")
-SESSION = Session(engine)
+engine = create_engine(DB_URL, echo=True)
