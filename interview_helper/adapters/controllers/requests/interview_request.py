0edecc08b7848bc2c9e58e5cefeecd2917bed2b9
nika.lee <dltndud9137@naver.com>
2024-03-11 21:30:56 +0900

    feat: add classmethod for converting between entity and dto

--- interview_helper/adapters/controllers/requests/interview_request.py
+++ interview_helper/adapters/controllers/requests/interview_request.py
@@ -5,6 +5,7 @@ from pydantic import BaseModel
 from interview_helper.adapters.controllers.requests.qna_sequence_request import (
     QnaSequenceRequestDTO,
 )
+from interview_helper.domain.interview.interview import Interview
 
 
 class InterviewRequestDTO(BaseModel):
@@ -13,3 +14,16 @@ class InterviewRequestDTO(BaseModel):
     tags: List[str]
     description: str
     qna_sequences: List[QnaSequenceRequestDTO]
+
+    @classmethod
+    def to_entity(cls, interview_request: "InterviewRequestDTO") -> Interview:
+        return Interview(
+            user=User(user_id=interview_request.registered_user_id),
+            title=interview_request.title,
+            tags=[Tag(tag_name=tag) for tag in interview_request.tags],
+            description=interview_request.description,
+            qna_sequences=[
+                QnaSequence.to_entity(qna_sequence)
+                for qna_sequence in interview_request.qna_sequences
+            ],
+        )
