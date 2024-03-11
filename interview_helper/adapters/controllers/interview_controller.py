6df8f9834212eafa5f4f87c440446f0ac77079b0
nika.lee <dltndud9137@naver.com>
2024-03-11 21:32:47 +0900

    feat: add dependency module for di and apply controller

--- interview_helper/adapters/controllers/interview_controller.py
+++ interview_helper/adapters/controllers/interview_controller.py
@@ -1,26 +1,29 @@
 from fastapi.routing import APIRouter
 
-from interview_helper.adapters.responses import InterviewResponse
-from interview_helper.adapters.requests import InterviewRequest
-from interview_helper.app.ports.interview_inbound_port import InterviewInboundPort
+from ..inbound_ports.interview_inbound_port import InterviewInboundPort
+from .requests.interview_request import InterviewRequestDTO
+from .responses.interview_response import (
+    CreateInterviewResponseDTO,
+    InterviewResponseDTO,
+)
 
-router = APIRouter()
+router = APIRouter(prefix="/interviews")
 interview_service = InterviewInboundPort()
 
 
-@router.get("/interviews")
+@router.get
 async def get_interviews():
     interviews = interview_service.get_all()
-    return InterviewResponse.from_list(interviews)
+    return InterviewResponseDTO.from_interviews(interviews)
 
 
-@router.get("/interviews/{interview_id}")
+@router.get("/{interview_id}")
 async def get_interview(interview_id: int):
     interview = interview_service.get_by_id(interview_id)
-    return InterviewResponse.from(interview)
+    return InterviewResponseDTO.from_entity(interview)
 
 
-@router.post("/interviews")
-async def create_interview(request: InterviewRequest):
-    interview_id = interview_service.create(InterviewRequest.to_entity(request))
-    return InterviewResponse.from_id(interview_id)
+@router.post
+async def create_interview(request: InterviewRequestDTO):
+    interview_id = interview_service.create(InterviewRequestDTO.to_entity(request))
+    return CreateInterviewResponseDTO(interview_id=interview_id)
---
+++ interview_helper/config/dependencies.py
@@ -0,0 +1,5 @@
+from interview_helper.app.outbound_ports.interview_outbound_port import InterviewOutboundPort
+from interview_helper.adapters.infrastructures.repository.inter
+
+def get_repository() -> InterviewOutboundPort:
+    return 
